dfddadb43202ef51a92a59f2fc18e77519e8437d
LiaIndex <54170468+LiaIndex@users.noreply.github.com>
2019-08-23 12:08:32 +0200

    Update main.js

--- Hojita/main.js
+++ Hojita/main.js
@@ -1,43 +1,53 @@
+/*
+Author: Lia de Belda
+Using P5 Library for JavaScript
+*/
+
 let x=0;
 let y=0;
 
+//width and height of the canvas
+let w = 1000;
+let h = 1000;
+
 function setup(){
-    createCanvas(1000,1000);
-    
+    createCanvas(w,h);
     draw();
 }
 
 function draw(){
-    translate(500,500);
+    translate(w/2,h/2);
+    
+    //set the probability of the function to follow
     let r = aleatorio(1,100);
     stroke(0);
-    print(r);
+    //print(r);
     let auxX = x;
     let auxY = y;
     
+        //1%
         if(r==1){
-            //print("estoy en la funcion 1 opcion");
+            
             funcion1(auxY);
-            print("caso  1 x vale: ",x," e y vale: "+y);
             set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
             
         }
+        //8%
         else if(r>6 && r<15){
-            //print("estoy en la funcion 2 opcion");
+            
             funcion3(auxX,auxY);
-            print("caso  2 x vale: ",x," e y vale: "+y);
             set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
         }
+        //9%
         else if(r>=15 && r<=23) {
-            //print("estoy en la funcion 3 opcion");
+            
             funcion4(auxX,auxY);
-            print("caso  3 x vale: ",x," e y vale: "+y);
             set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
         }
+        //82%
         else if(r>23 || (r<6 && r>1)){
-            //print("estoy en la funcion 4  opcion");
+            
             funcion2(auxX,auxY);
-            print("caso  4 x vale: ",x," e y vale: "+y);
             set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
         }
         updatePixels();
@@ -60,6 +70,7 @@ function funcion4(a,b){
     x=-0.15*a+0.28*b;
     y=0.26*a+0.24*b+0.44;
 }
+
 function aleatorio(minimo,maximo){
     return Math.floor(Math.random() * ((maximo+1)-minimo)+minimo);
   }
