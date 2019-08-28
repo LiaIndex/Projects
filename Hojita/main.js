a1d7709c930e2e280b7870a895d51a417c688cb3
LiaIndex <54170468+LiaIndex@users.noreply.github.com>
2019-08-28 11:31:32 +0200

    Update main.js

--- Hojita/main.js
+++ Hojita/main.js
@@ -1,5 +1,7 @@
 /*
 Author: Lia de Belda
+Barnsley fern fractal.
+see also : https://en.wikipedia.org/wiki/Barnsley_fern
 Using P5 Library for JavaScript
 */
 
@@ -7,8 +9,8 @@ let x=0;
 let y=0;
 
 //width and height of the canvas
-let w = 1000;
-let h = 1000;
+let w = 750;
+let h = 750;
 
 function setup(){
     createCanvas(w,h);
@@ -16,61 +18,58 @@ function setup(){
 }
 
 function draw(){
-    translate(w/2,h/2);
+    //translate(w/2,h/2);
     
-    //set the probability of the function to follow
-    let r = aleatorio(1,100);
-    stroke(0);
-    //print(r);
-    let auxX = x;
-    let auxY = y;
+    for(let i=0; i<150; i++){
+      //set the probability of the function to follow
+      let r = myRnd(1,100);
+      stroke(0);
+      strokeWeight(2);
+      
+      let auxX = x;
+      let auxY = y;
     
         //1%
         if(r==1){
-            
-            funcion1(auxY);
-            set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
-            
+            f1(auxY);
+            point(map(x, -2.5, 3, 0, w), map(y, 0, 10, 0, h));
         }
         //8%
         else if(r>6 && r<15){
-            
-            funcion3(auxX,auxY);
-            set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
+            f3(auxX,auxY);
+            point(map(x, -2.5, 3, 0, w), map(y, 0, 10, 0, h));
         }
         //9%
         else if(r>=15 && r<=23) {
-            
-            funcion4(auxX,auxY);
-            set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
+            f4(auxX,auxY);
+            point(map(x, -2.5, 3, 0, w), map(y, 0, 10, 0, h));
         }
         //82%
         else if(r>23 || (r<6 && r>1)){
-            
-            funcion2(auxX,auxY);
-            set(map(x, -2.5, 3, 0, 1000), map(y, 0, 10, 0, 1000),color(0));
+            f2(auxX,auxY);
+            point(map(x, -2.5, 3, 0, w), map(y, 0, 10, 0, h));
         }
-        updatePixels();
-     
+        //updatePixels();
+    }
 }
 
-function funcion1(b){
+function f1(b){
     x=0;
     y=0.16*b;
 }
-function funcion2(a,b){
+function f2(a,b){
     x=0.85*a+0.04*b;
     y=-0.04*a+0.85*b+1.6;
 }
-function funcion3(a,b){
+function f3(a,b){
     x=0.2*a-0.26*b;
     y=0.23*a+0.22*b+1.6;
 }
-function funcion4(a,b){
+function f4(a,b){
     x=-0.15*a+0.28*b;
     y=0.26*a+0.24*b+0.44;
 }
 
-function aleatorio(minimo,maximo){
+function myRnd(minimo,maximo){
     return Math.floor(Math.random() * ((maximo+1)-minimo)+minimo);
   }
