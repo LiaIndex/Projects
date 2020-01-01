4de38f2e6d45a5e76c0ecb761099fd5ad445ecdd
LiaIndex <54170468+LiaIndex@users.noreply.github.com>
2020-01-01 19:54:15 +0100

    Create spirograph.js

---
+++ sin&cos_examples/spirograph.js
@@ -0,0 +1,107 @@
+
+let width = 400;
+let height = 400;
+
+let radio =width/4;
+/*
+let angulo1 = 0.0;
+let angulo2 = 0.0; 
+let angulo3 = 0.0;
+*/
+
+let toplot = [];
+
+
+let circulos = 23;
+let angulos = [];
+let centers = [];
+
+function setup() {
+ createCanvas(width, height);
+  
+  for(let i=0; i<circulos; i++){
+    angulos.push(0.0);
+    centers.push({x:0,y:0});
+  }
+}
+
+function draw() {
+  
+  background(220);
+  
+  
+  stroke(0);
+  noFill();
+  
+  //original circle
+  translate(width/2,height/2);
+  circle(0,0,radio);
+  
+  for(let k=0; k<circulos; k++){
+    
+    let radioi = ( radio / ( 2*(k+1) ) );
+    
+    //nuevo centro para el siguiente orbital
+    let xi = cos(angulos[k]) * radioi;
+    let yi = sin(angulos[k]) * radioi;
+    
+    if(k>0){
+      xi += centers[k-1].x;
+      yi += centers[k-1].y;
+    }
+    
+    
+    centers[k] = {x:xi, y:yi};
+    circle(xi,yi,radioi);
+    angulos[k] += (PI / 3000) * (k+1)*10/50  ;
+    
+    if(k==circulos-1){
+      toplot.push({x:xi,y:yi});
+      //console.log(xi,yi);
+    }
+  }
+  
+  stroke(color(250,0,0));
+  for(let i=0; i<toplot.length; i++){
+    point(toplot[i].x,toplot[i].y);
+  }
+  
+  /*
+  circle(0,0,radio);
+  
+  //x and y position of the orbital's center
+  let x1 = cos(angulo1) * radio/2;
+  let y1 = sin(angulo1) * radio/2;
+  circle(x1,y1,radio/2);
+  
+  //add angle
+  angulo1 += PI/500;
+  
+  
+  //x and y of the second orbital
+  
+  let x2 = ( cos(angulo2) * radio/4 ) + x1;
+  let y2 = ( sin(angulo2) * radio/4 ) + y1;
+  
+  circle(x2,y2,radio/4);
+  
+  //add to the 2nd angle
+  angulo2 += PI/250;
+  
+  //plot trayectory
+  let x_ = ( cos(angulo3) * radio/8) + x2;
+  let y_ = ( sin(angulo3) * radio/8) + y2;
+  
+  
+  toplot.push({x:x_,y:y_});
+  angulo3 += PI/125;
+  
+  if(angulo1 >= 360) angulo1=0;
+  if(angulo2 >= 360) angulo2=0;
+  if(angulo3 >= 360) angulo3=0;
+  
+  stroke(color(250,0,0));
+  for(let i=0; i<toplot.length; i++){
+    point(toplot[i].x,toplot[i].y);
+  }*/
+}
